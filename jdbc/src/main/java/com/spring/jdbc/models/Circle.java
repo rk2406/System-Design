f186368767eedb68b1059be28e389803720d09a2
rupasa <ritishakarmakar@gmail.com>
2022-08-21 19:19:44 +0530

    removed hibernate code

--- jdbc/src/main/java/com/spring/jdbc/models/Circle.java
+++ jdbc/src/main/java/com/spring/jdbc/models/Circle.java
@@ -2,13 +2,8 @@ package com.spring.jdbc.models;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
 @Data
-@Entity
 public class Circle {
-    @Id
     private int id;
     private String name;
 
